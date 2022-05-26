ce23f6a5b34439d81246c61f5251028ebdc8340d
VukZh <vukfx@inbox.ru>
2022-05-27 01:18:21 +0600

    add source-map-loader and 2 modes set

--- react_js_course/webpack.config.js
+++ react_js_course/webpack.config.js
@@ -3,53 +3,67 @@ const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, './src/index.js'),
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },{
-        test: /\.(png|gif|ico|jpg|jpeg)$/,
-        use: [{
-          loader: "file-loader",
-          options: {
-            outputPath: "images",
-            name: '[name]-[sha1:hash:7].[ext]'
-          }
-        }]
-      },{
-        test: /\.(ttf|otf|eot|woff|woff2)$/,
-        use: [{
-          loader: "file-loader",
-          options: {
-            outputPath: "fonts",
-            name: '[name].[ext]'
-          }
-        }]
-      }
-      ,{
-        test: /\.(css)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      }
+module.exports = (env = {}) => {
+
+  const isProd = env.production;
+  const isDev = env.development;
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
     ]
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "public/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style-[hash:8].css'
-    })
-  ],
-  devServer: {
-    open: true
+  }
+
+  return {
+    mode: isProd ? "production" : isDev && "development",
+    devtool: "source-map",
+    entry: path.resolve(__dirname, './src/index.js'),
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },{
+          test: /\.(png|gif|ico|jpg|jpeg)$/,
+          use: [{
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }]
+        },{
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [{
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts",
+              name: '[name].[ext]'
+            }
+          }]
+        }
+        ,{
+          test: /\.(css)$/,
+          use: getStyleLoaders(),
+        }
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "public/index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style-[hash:8].css'
+      })
+    ],
+    devServer: {
+      open: true
+    }
   }
 }
